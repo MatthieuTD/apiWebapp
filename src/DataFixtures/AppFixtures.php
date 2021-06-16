345a9ec4349dc7d06e2294162406a841d0d85fae
Lahaxe Hugo <lahaxeh@gmail.com>
2021-06-16 10:36:52 +0200

    Ajout fixture pour questions de tous les types

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -56,8 +56,44 @@ class AppFixtures extends Fixture
         $manager->persist($reponse_option2);
         $manager->persist($reponse_option3);
 
+        
+        $question_simple = new Question();
+        $question_simple->setName("Quel est votre nom ?");
+        $question_simple->setType($type_text);
 
+        $reponse2_option = new QuestionOption();
+        $reponse2_option->addQuestion($question_simple);
+        
+        $manager->persist($question_simple);
+        $manager->persist($reponse2_option);
 
+        $question_bool = new Question();
+        $question_bool->setName("Quel est votre tranche d'âge ?");
+        $question_bool->setType($type_bool);
+
+        $reponse3_option1 = new QuestionOption();
+        $reponse3_option1->setName("20-30 ans");
+        $reponse3_option1->addQuestion($question_bool);
+
+        $reponse3_option2 = new QuestionOption();
+        $reponse3_option2->setName("30-40 ans");
+        $reponse3_option2->addQuestion($question_bool);
+
+        $reponse3_option3 = new QuestionOption();
+        $reponse3_option3->setName("40-50 ans");
+        $reponse3_option3->addQuestion($question_bool);
+
+        $reponse3_option4 = new QuestionOption();
+        $reponse3_option4->setName("50+ ans");
+        $reponse3_option4->addQuestion($question_bool);
+
+        
+        $manager->persist($question_bool);
+        $manager->persist($reponse3_option1);
+        $manager->persist($reponse3_option2);
+        $manager->persist($reponse3_option3);
+        $manager->persist($reponse3_option4);
+        
 
         $manager->flush();
     }
