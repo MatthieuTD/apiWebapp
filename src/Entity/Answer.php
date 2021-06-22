9ff33c62b18f1638cd027d15c64d94533d170f52
Matthieu THOLOT-DECHENE <mtholot19@gmail.com>
2021-06-22 16:52:00 +0200

    Ajout des img

--- src/Entity/Answer.php
+++ src/Entity/Answer.php
@@ -32,7 +32,7 @@ class Answer
     private $answerOptions;
 
     /**
-     * @ORM\Column(type="string", length=1000, nullable=true)
+     * @ORM\Column(type="string", length=1000)
      */
     private $token;
 
